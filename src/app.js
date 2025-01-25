62884af7871a2d219afc4ada5bd2e7cf8a618ad9
Josephglz <joseph220199@gmail.com>
2025-01-24 23:01:00 -0600

    feat: integrate product routes and enable JSON parsing in the API

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
 import { runMigrations } from './utils/migrations.js';
+import productRoutes from './routes/productRoutes.js';
+
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.status(200).json({message: "API is running..."}));
+app.use('/api', productRoutes)
 
 runMigrations();
 
