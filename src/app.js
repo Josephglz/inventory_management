08be75d30f4d210540243b760eb354e51024b21b
Josephglz <joseph220199@gmail.com>
2025-01-25 23:10:58 -0600

    refactor: restructure server startup logic and export app instance for testing

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 import { runMigrations } from './utils/migrations.js';
 import productRoutes from './routes/productRoutes.js';
 
-
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -12,8 +11,16 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.status(200).json({message: "API is running..."}));
-app.use('/api', productRoutes)
+app.use('/api', productRoutes);
 
 runMigrations();
 
-app.listen(PORT, () => console.log(`[SERVER]: Running on http://localhost:${PORT}`));
+const startServer = (appInstance, port) => {
+  appInstance.listen(port, () => console.log(`[SERVER]: Running on http://localhost:${port}`));
+};
+
+if (process.env.NODE_ENV !== 'test') {
+  startServer(app, PORT);
+}
+
+export default app;
