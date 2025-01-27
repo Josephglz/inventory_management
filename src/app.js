8f3a74ace1534de35077734e9da3d0b6bd16cf8a
Josephglz <joseph220199@gmail.com>
2025-01-27 14:04:13 -0600

    feat: add stock routes to the API

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 import { runMigrations } from './utils/migrations.js';
 import productRoutes from './routes/productRoutes.js';
+import stockRoutes from './routes/stockRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => res.status(200).json({message: "API is running..."}));
 app.use('/api', productRoutes);
+app.use('/api', stockRoutes);
 
 runMigrations();
 
